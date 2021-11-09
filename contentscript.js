b8ddd56e70f81a6b275e1f1caea28a0785b126e5
吴延博 <786021717@qq.com>
2021-11-09 15:11:23 +0800

    Update contentscript.js

--- contentscript.js
+++ contentscript.js
@@ -1,24 +1,18 @@
 // see https://stackoverflow.com/questions/23202136/changing-navigator-useragent-using-chrome-extension
 var actualCode =  '(' + function() {
-    // Get the correct platform for navigator.userAgent 
-    var candidates = ["Android", "iPhone", "iPod", "iPad"];
-    var modifiedPlatform;
-    for(var maybe of candidates) {
-        if(navigator.userAgent.match(maybe)) {
-            modifiedPlatform = maybe;
-            break;
-        }
-    }
-
-    if(!modifiedPlatform) {
-        return;
-    }
-    
-    // Reset platform
-    Object.defineProperty(navigator, 'platform', {
-        get: function(){
-            return modifiedPlatform;
-        }});
+    // Reset navigator
+    Object.defineProperties(navigator, {
+      userAgent: {
+        get: function () {
+          return 'iPhone';
+        },
+      },
+      platform: {
+        get: function () {
+          return 'iPhone';
+        },
+      },
+    });
 } + ')();';
 
 // Inject custom js
